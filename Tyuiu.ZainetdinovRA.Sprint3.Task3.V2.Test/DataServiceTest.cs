a2d4ddae17994b6da6d79bf6e44f36af58de126b
Tyuiu.ZainetdinovRA@outlook.com <rosiper123@icloud.com>
2024-11-21 17:11:26 +0500

    Task3

---
+++ Tyuiu.ZainetdinovRA.Sprint3.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ZainetdinovRA.Sprint3.Task3.V2.Lib;
+namespace Tyuiu.ZainetdinovRA.Sprint3.Task3.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ExpressionValid()
+        {
+            DataService ds = new DataService();
+            string value = "zzzzzz";
+            char c = 'z';
+            int wait = 6;
+            int result = ds.GetMaxCharCount(value, c);
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.ZainetdinovRA.Sprint3.Task3.V2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using Tyuiu.ZainetdinovRA.Sprint3.Task3.V2.Lib;
+namespace Tyuiu.ZainetdinovRA.Sprint3.Task3.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "Спринт #3 | Выполнил: Zainetdinov R. A. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Зайнетдинов Руслан Александрович | ИИПБ-24-2                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* НИспользуя цикл foreach подсчитать максимальное количество букв z,      *");
+            Console.WriteLine("* находящихся на соседних позициях в строке: asdzzz vfvfzz v gthvz        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "asdzzz vfvfzz v gthvz";
+            char c = 'z';
+            int result = ds.GetMaxCharCount(value, c);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Количество вхождения z в строку равно: {result}a");
+        }
+    }
+}
